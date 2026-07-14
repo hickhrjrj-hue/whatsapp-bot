88f999f7d33ced54cc1b31fe7c82d3f173fc1981
hickhrjrj-hue <hickhrjrj@gmail.com>
2026-07-14 12:56:35 +0800

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -2,10 +2,8 @@
 # exit on error
 set -o errexit
 
-# 1. Install your project's npm dependencies
 npm install
 
-# 2. Download and cache Chrome specifically for Puppeteer on Render
 PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer
 mkdir -p $PUPPETEER_CACHE_DIR
 npx puppeteer browsers install chrome
